5ec62928bf40b3ceee623d620050340ed0874464
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-25 23:46:26 -0500

    set assertion for molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,21 +6,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-UNZIP_BINARY_PATH = '/usr/bin/unzip'
-PACKAGE = 'unzip'
-
-
 def test_unzip_package_installed(host):
-    host.package("PACKAGE").is_installed
+    assert host.package("unzip").is_installed
 
 
 def test_unzip_binary_exists(host):
-    host.file('UNZIP_BINARY_PATH').exists
+    assert host.file('/usr/bin/unzip').exists
 
 
 def test_unzip_binary_file(host):
-    host.file('UNZIP_BINARY_PATH').is_file
+    assert host.file('/usr/bin/unzip').is_file
 
 
 def test_unzip_binary_whereis(host):
-    host.check_output('whereis unzip') == UNZIP_BINARY_PATH
+    assert host.check_output('whereis unzip') == '/usr/bin/unzip'
